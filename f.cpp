5c3f5f74fa4dc5da2379e5ff2faf21a755f93d59
yonelyvan <yonelyvan@gmail.com>
2018-07-03 15:10:52 -0500

    s

---
+++ f.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<float> vf;
+typedef vector<vi> vvi;
+
+
+typedef struct{
+	double vi;
+	double fitness;
+}individuo;
+
+
+
+
+int main(){
+
+	return 0;
+}
